4b1209d15acaf25265160b4523839ecb9c3a611f
srikanthpragada <srikanthpragada@gmail.com>
2019-04-05 09:29:49 +0530

    05-APR

---
+++ Factors.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpDemo
+{
+    class Factors
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Number is missing!");
+                return; // stop Main() 
+            }
+
+            int num = Int32.Parse(args[0]);
+
+            for(int i = 2; i <= num/2; i ++)
+            {
+                if (num % i == 0)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpDemo
+{
+    class Product
+    {
+        // Instance variables 
+        private int id;
+        private string name;
+        private double price;
+        // Constructor 
+        public Product(int prodid, string prodname, double prodprice=0)
+        {
+            id = prodid;
+            name = prodname;
+            price = prodprice; 
+        }
+        // Method
+        public void Print()
+        {
+            Console.WriteLine($"{id} - {name} - {price}");
+        }
+    }
+    class TestProduct
+    {
+        static void Main(string[] args)
+        {
+            Product p1;  // object reference
+
+            p1 = new Product(1,"Dell Laptop");  // create object 
+            // p1.id = 101;
+            p1.Print();
+
+            Product p2 = new Product(2, "Bose Headphones", 20000);
+            p2.Print(); 
+
+        }
+    }
+}

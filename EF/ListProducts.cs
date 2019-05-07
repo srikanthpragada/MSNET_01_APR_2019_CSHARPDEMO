3b09ed23001d277d513e5ff917c76a4c93e5145d
srikanthpragada <srikanthpragada@gmail.com>
2019-05-07 09:28:16 +0530

    07-MAY

---
+++ EF/ListProducts.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpDemo.EF
+{
+    class ListProducts
+    {
+        static void Main(string[] args)
+        {
+            var ctx = new CatalogEntities();
+            // ctx.Database.Log = Console.WriteLine;
+
+            foreach(var p in ctx.products)
+            {
+                Console.WriteLine($"{p.prodname} - {p.sales.Sum( s => s.amount)}");
+            }
+        }
+    }
+    
+}
---
+++ EF/ListSales.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpDemo.EF
+{
+    class ListSales
+    {
+        static void Main(string[] args)
+        {
+            var ctx = new CatalogEntities();
+            ctx.Database.Log = Console.WriteLine;
+
+            foreach(sale s in ctx.sales.Include("product"))
+            {
+                Console.WriteLine($"{s.invno} - {s.product.prodname} - {s.amount}");
+            }
+        }
+    }
+}
